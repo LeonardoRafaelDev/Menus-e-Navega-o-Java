dc76fbbd641bf8d28fc2a0b956bfcb8769409a74
LeonardoHe4rt <LeonardoRafaeldev@gmail.com>
2022-12-19 15:23:18 -0300

    Add files via upload

---
+++ menuinteracao/mavenproject1/src/main/java/com/mycompany/mavenproject1/aluno.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author leleo
+ */
+public class aluno {
+    String nome;
+    String serie;
+    String instituicao;
+    
+    public aluno (){
+        this.nome = nome;
+        this.serie = serie;
+        this.instituicao = instituicao;
+    }
+}
